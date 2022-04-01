b1c2023421a26c0521ca018764b2dc9dc2486fe5
Nicety <78967461+Nicetyone@users.noreply.github.com>
2022-04-01 13:37:11 +0200

    version 1.0

---
+++ nty-load/js/config.js
@@ -0,0 +1,7 @@
+
+var config = {
+    music: ["QBjYQhhmzO8", "9UMxZofMNbA", "36YnV9STBqc"], // <-- Links of Songs in order
+    songnames: ["Late Night Lounge", "Chillout Lounge", "Good Life Radio"], // <-- Name of Songs in order
+    musicVolume: 5, // volume should be under 50% to not cause earrape :)
+    enableMusic: true
+}
---
+++ nty-load/js/showRealTime.js
@@ -0,0 +1,24 @@
+// function by Nigol.
+
+function startTime() {
+  var monthNames = ["Styczeń","Luty","Marzec","Kwiecień","Maj","Czerwiec","Lipiec","Sierpień","Wrzesień","Październik","Listopad","Grudzeń"];
+
+  var today = new Date();
+  var h = today.getHours();
+  if (h < 10) { h = '0'+h; } 
+  var m = today.getMinutes(); 
+  var s = today.getSeconds();
+  // var year = today.getFullYear();
+  var monthIndex = today.getMonth();
+  var month = monthNames[monthIndex];
+  var day = today.getDate();  
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("showRealTime").innerHTML = `${month} ${day} | ${h}:${m}:${s}`;
+  setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
